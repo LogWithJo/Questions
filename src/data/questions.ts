8fc11e1895d6e01c19dfb60c264f6b30d98627d5
youssef <ya53114808@gmail.com>
2026-08-13 16:17:01 +0300

    cutting two 2 parts

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,9 +1,8 @@
 import type { Question } from "@/types";
-import data from "../../data.json" with { type: "json" };
+import data from "../../data1.json" with { type: "json" };
 
 export const questions: Question[] = data as Question[];
 
 export function getQuestionById(id: number): Question | undefined {
-  return questions.find((q) => q.id === id);
+	return questions.find((q) => q.id === id);
 }
-
